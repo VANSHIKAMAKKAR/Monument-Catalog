1993ec2ffe64fc72b0cd3287f45effbb8a363b14
unknown <Vanshika Makkar>
2019-04-05 12:36:08 +0530

    Initial Commit

---
+++ database_setup.py
@@ -0,0 +1,61 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Places(Base):
+    __tablename__ = 'places'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class PopularLocations(Base):
+    __tablename__ = 'popular_locations'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(2500))
+    year = Column(Integer)
+    founder = Column(String(250))
+    places_id = Column(Integer, ForeignKey('places.id'))
+    places = relationship(Places)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+
+
+engine = create_engine('sqlite:///itemcatalogappwithlogin.db')
+
+
+Base.metadata.create_all(engine)
